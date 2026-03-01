59638cd7a3380c767b5692a4db641c4a72ab2c6d
tush00nka <endline00@ya.ru>
2026-03-02 01:01:51 +0300

    Menu and MenuItem

--- sources/application.h
+++ sources/application.h
@@ -4,6 +4,7 @@
 #include "data_pool.h"
 #include "options.h"
 #include <functional>
+#include <unordered_map>
 
 const size_t kInputVectorArgc = 5;
 const size_t kAddVectorArgc = 6;
@@ -19,9 +20,27 @@ void help(DataPool& data, std::vector<std::string>& commandArgs, Options& opts, 
 
 void popVec(DataPool& data, std::vector<std::string>& commandArgs, Options& opts, IConsole& console);
 
-static void clear(IConsole& console)
+inline void clear(IConsole& console)
 {
 	console.print("\033[2J\033[1;1H"); // clear the console
 }
 
 int runApplication(Options& opts, DataPool& data, IConsole& console);
+
+class MenuItem {
+private:
+	command _command;
+public:
+	MenuItem() = default;
+	explicit MenuItem(command command);
+	bool execute(std::vector<std::string>& command_args, Options& opts, DataPool& data, IConsole& console);
+};
+
+class Menu {
+private:
+	std::unordered_map<std::string, MenuItem> _items;
+public:
+	Menu();
+	bool execute(const std::string& command_text, std::vector<std::string>& command_args, Options& opts, DataPool& data, IConsole& console);
+	bool exists(const std::string& command_text);
+};
